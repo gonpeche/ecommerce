8f11aefd108424bfea2ce4fe9a5eb8875cd39080
diegofernandezfontana <diegofernandezfontana@gmail.com>
2018-10-30 18:58:51 -0300

    giladas

--- back/index.js
+++ back/index.js
@@ -32,11 +32,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('../front/dist'));
 
 app.get('/', function(req, res) {
-    models.User.create({
-        nombre: 'tuvieja',
-        apellido: 'uno',
-        email: 'aaaasd@gmail.com',
+    
+    models.Producto.create({
+        nombre: 'MAC BOOK PRO',
+        descripcion: 'Producto de mac sobrevalorado',
+        foto: 'https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/m/bp/mbp15touch/space/mbp15touch-space-select-201807?wid=904&hei=840&fmt=jpeg&qlt=95&.v=1529520056969m',
+        categoria: ['Pcs', 'Notebook'],
+        disponibilidad: true,
     })
+    models.Producto.create({
+        nombre: 'Ipad',
+        descripcion: 'Un celu grande',
+        foto: 'https://http2.mlstatic.com/ipad-pro-129-64-gb-gris-ipad-pro-D_NQ_NP_967249-MLA26076837669_092017-F.webp',
+        categoria: ['Ipad'],
+        disponibilidad: false,
+    })
+
+    
     .catch((error) => console.log(error))
     .then(data => {
         res.sendFile(path.resolve('../front/index.html'));
