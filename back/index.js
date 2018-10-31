493867bbfbdf73feadd5dc123ab3ee22f5c7bdec
diegofernandezfontana <diegofernandezfontana@gmail.com>
2018-10-31 11:46:43 -0300

    Sync terminado

--- back/index.js
+++ back/index.js
@@ -5,55 +5,18 @@ var path = require('path');
 const db = require('./models/db');
 const models = require('./models/index').modelos;
 
-/*
-models.User.sync({ force: false })
-    .then(function () {
-        return models.Producto.sync({ force: false });
-    })
-    .then(function () {
-        return models.Review.sync({ force: false });
-    })
-    .then(function () {
-        return models.Venta.sync({ force: false });
-    })
-    .then(function () {
-        return models.Categoria.sync({ force: false });
-    })
-    .then(function () {
-        app.listen('3000', function () {
-            console.log('listening at 3000');
-        });
-    })
-    .catch(console.error);
-*/
 
 db.sync({force: true})
 .then(function () {
-    app.listen('3001', function () {
-        console.log('listening at 3001');
+    app.listen('3000', function () {
+        console.log('listening at 3000');
     });
 })
 
-
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static('../front/dist'));
 
 app.get('/', function (req, res) {
-   /* models.Venta.create({
-        producto: ['JUGUETE'],
-        estado: 'procesado',
-        fecha: 20 - 10 - 18,
-        importe: 2000,
-        direccion: 'av tuvieja 3180',
-        email: 'tuvieja69@gmail.com',
-        userId: 1
-    })
-        .catch((error) => console.log(error))
-        .then(data => {
-            */
-            res.sendFile(path.resolve('../front/index.html'));
-        //})
+    res.sendFile(path.resolve('../front/index.html'));
 });
